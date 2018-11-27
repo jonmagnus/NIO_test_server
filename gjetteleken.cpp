741b440377a5b199dbc4cc96fd5beda8695a9adb
Jon-Magnus Rosenblad <jonmagnusr@gmail.com>
2018-11-27 08:19:08 +0100

    Added gjetteleken.cpp

---
+++ gjetteleken.cpp
@@ -0,0 +1,20 @@
+#include <cstdio>
+using namespace std;
+
+int main()
+{
+	int N,M;
+	while (scanf("%d %d ", &N,&M) != EOF)
+	{
+		while (M--)
+		{
+			int x;
+			scanf("%d ", &x);
+			if (x < N)	printf("FOR LITE\n");
+			else if (x > N)	printf("FOR MYE\n");
+			else		printf("RIKTIG\n");
+		}
+	}
+
+	return 0;
+}
